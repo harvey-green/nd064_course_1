642afa5411415934db4a45063f2a405d1eb6c744
Harvey Green <greenh@iinet.net.au>
2021-11-20 21:00:38 +0800

    Updated the metrics endpoint to show the dynamic data for connection count and number of posts.

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -4,11 +4,16 @@ import logging
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+# global variable used to count the number of db connections
+ccount = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global ccount
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    ccount = ccount + 1
     return connection
 
 # Function to get a post using its ID
@@ -79,10 +84,14 @@ def healthcheck():
 
 @app.route('/metrics')
 def metrics():
-    dbConnectionCount = 1
-    postCount = 7
+    global ccount
+    connection = get_db_connection()
+    postCountQuery = connection.execute("select count(*) from posts")
+    postCountData = postCountQuery.fetchone()
+    postCount = postCountData[0]
+    
     response = app.response_class(
-            response=json.dumps({"db_connection_count":dbConnectionCount,"post_count":postCount}),
+            response=json.dumps({"db_connection_count":ccount,"post_count":postCount}),
             status=200,
             mimetype='application/json'
     )
